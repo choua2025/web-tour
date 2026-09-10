fdca9c71d5e912b16358da74cf0312f51efc2b03
choua2025 <vchoua05@gmail.com>
2026-09-10 10:59:35 +0700

    test

--- booking-tratvel/src/routes/test.routes.js
+++ booking-tratvel/src/routes/test.routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post('/test', async (req, res) => {
+router.get('/test', async (req, res) => {
     res.json({ message: 'Test endpoint is working' });
 });
 
