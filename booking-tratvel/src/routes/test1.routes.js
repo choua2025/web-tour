98a35aaee669f1011f19ec9aa4efcb60fea7fb76
choua2025 <vchoua05@gmail.com>
2026-09-14 09:51:19 +0700

    test

--- booking-tratvel/src/routes/test1.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-
-const router = Router();
-
-router.get('/test1', async (req, res) => {
-    res.json({ message: 'Test1 endpoint is working1' });
-});
-
-export default router;
