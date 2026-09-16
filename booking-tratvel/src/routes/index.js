7246dfe9dab46b7ddc54887da6216b2a54478148
choua2025 <vchoua05@gmail.com>
2026-09-16 14:35:25 +0700

    test

--- booking-tratvel/src/routes/index.js
+++ booking-tratvel/src/routes/index.js
@@ -12,7 +12,7 @@ import reviewRoutes from './review.routes.js';
 import contactRoutes from './contact.routes.js';
 import eventRoutes from './events.routes.js';
 import healthRoutes from './health.routes.js';
-import loginRoutes from './login.routes.js';
+
 
 
 const router = Router();
@@ -38,7 +38,7 @@ router.use('/reviews', reviewRoutes);
 router.use('/contact', contactRoutes);
 router.use('/events', eventRoutes);
 router.use('/health', healthRoutes);
-router.use('/login', loginRoutes);
+
 
 
 export default router;
